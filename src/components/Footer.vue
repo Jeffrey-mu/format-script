30df5a4eaf2f6630aa3435ca6cad83f3f3046456
Jeffrey-mu <1115378579@qq.com>
2022-07-12 23:32:15 +0800

    style：修改顶部margin

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@ const toggleLocales = () => {
 </script>
 
 <template>
-  <nav text-xl mt-6>
+  <nav text-xl mb-6>
     <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
       <div i="carbon-sun dark:carbon-moon" />
     </button>
