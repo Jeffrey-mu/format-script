18a887338e8e8b18943936a2ba91507584b3ea0b
mr-wang1009 <1115378579@qq.com>
2022-07-13 10:41:33 +0800

    feat：添加head代码

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,33 +1,5 @@
 <script setup lang="ts">
-import format from 'html-format'
 import { data } from '~/composables/data'
-const input = ref(data)
-const copyIndex = ref(-1)
-const newValue = computed(() => {
-  if (!input.value)
-    return ''
-  const reg = /www.*?_[0-9]\n/g
-  const tab: string[] = input.value.toString().match(reg)
-  // .map(el => el.replace('\n', ''))
-  const array = input.value.toString().split('\n').filter(Boolean)
-  const newData: { title: string; script: string }[] = []
-  const typeIndex = tab.map((el) => {
-    return array.findIndex(item => item === el.replace('\n', ''))
-  })
-  typeIndex.reduce((acc, idnex) => {
-    newData.push({
-      title: array[acc],
-      script: array.slice(acc + 1, idnex).join(''),
-    })
-    return acc = idnex
-  }, 0)
-  const len = typeIndex.length
-  newData.push({
-    title: array[typeIndex[len - 1]],
-    script: array.slice(typeIndex[len - 1] + 1).join(''),
-  })
-  return newData.filter(item => item.script)
-})
 enum TypeName {
   dp = '详情',
   hp = '首页',
@@ -37,6 +9,48 @@ enum TypeName {
 }
 const DP = 'dp'
 const ANCHOR = 'anchor'
+const input = ref(data)
+const copyIndex = ref(-1)
+const newValue = computed(() => {
+  if (!input.value)
+    return ''
+  const titleReg = /www.*?_[0-9]\n/g
+  const scriptReg = /<script\b[^>]*><\/script>/
+  const tab: string[] = input.value.toString().match(titleReg)
+  // .map(el => el.replace('\n', ''))
+  const array = input.value.toString().split('\n').filter(Boolean)
+  const newData: { title: string; script: string }[] = []
+  const typeIndex = tab.map((el) => {
+    return array.findIndex(item => item === el.replace('\n', ''))
+  })
+
+  ;(function () {
+    const len = typeIndex.length
+    const lastData = array.slice(typeIndex[len - 1] + 1).join('')
+    const headScript = lastData.match(scriptReg)[0]
+    newData.push({
+      title: 'head',
+      script: headScript,
+    },
+    {
+      title: `${DP}_head`,
+      script: headScript,
+    }, {
+      title: array[typeIndex[len - 1]],
+      script: lastData.replace(scriptReg, ''),
+    },
+    )
+    typeIndex.reduce((acc, idnex) => {
+      newData.push({
+        title: array[acc],
+        script: array.slice(acc + 1, idnex).join('').replace(scriptReg, ''),
+      })
+      return acc = idnex
+    }, 0)
+  }())
+  return newData.filter(item => item.script)
+})
+
 function formatTile(title: string) {
   const array = title.split('_')
   return `${array.find(el => el === DP) ? TypeName.dp : TypeName.hp}_${formatType(array)}`
@@ -50,30 +64,13 @@ function formatType(array: string[]): string {
   str.push(array.at(-1))
   return `${str.join('_')}`
 }
-function formatScript(script: string) {
-  console.log(format(script))
-
-  return format(script)
-}
 function copy(script: string, index: number) {
   const aux = document.createElement('input')
-
-  // 设置元素内容
   aux.setAttribute('value', script)
-
-  // 将元素插入页面进行调用
   document.body.appendChild(aux)
-
-  // 复制内容
   aux.select()
-
-  // 将内容复制到剪贴板
   document.execCommand('copy')
-
-  // 删除创建元素
   document.body.removeChild(aux)
-
-  // 提示
   copyIndex.value = index
 }
 </script>
@@ -85,12 +82,12 @@ function copy(script: string, index: number) {
       <ul>
         <li v-for="item, index in newValue" :key="index" border mb4 pxy b-rd>
           <h2 text-left font-600>
-            <span color-green>{{ formatTile(item.title) }}</span>
+            <span color-blue>{{ index + 1 }}：</span><span color-green>{{ formatTile(item.title) }}</span>
             <div class="icon-btn mx-2" float-right title="copy">
               <div i-carbon-copy :class="[index === copyIndex ? 'color-green' : '']" @click="copy(item.script, index)" />
             </div>
           </h2>
-          <p>{{ formatScript(item.script) }}</p>
+          <p>{{ item.script }}</p>
         </li>
       </ul>
     </div>
